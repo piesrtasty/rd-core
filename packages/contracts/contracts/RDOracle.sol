85f4c9c61591adb55c4aa3aa48f04487020fa832
Luke Hamilton <pies.eth@pm.me>
2025-05-16 14:45:57 -0400

    chore: new rd oracle files

---
+++ packages/contracts/contracts/RDOracle.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+// Note: If > 50% of tokens in pool are yield bearing must use rate provider for token
+//  https://docs.balancer.fi/partner-onboarding/onboarding-overview/rate-providers.html
+
+contract RDOracle {
+    constructor() {
+        // TODO: Implement RDOracle constructor
+    }
+}
