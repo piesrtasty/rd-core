c3a5d007cbf045f15bd5bae3149fecc45dca67d2
Luke Hamilton <pies.eth@pm.me>
2025-07-27 13:18:28 -0400

    feat: change target evm version to cancun to support mcopy

--- packages/contracts/hardhat.config.js
+++ packages/contracts/hardhat.config.js
@@ -66,7 +66,7 @@ module.exports = {
       {
         version: "0.8.24",
         settings: {
-          evmVersion: "paris",
+          evmVersion: "cancun",
           optimizer: {
             enabled: true,
             runs: 100
